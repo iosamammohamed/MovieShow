5ba682e3d1cc793cc17170a0a6e534778180cbf6
Osama Magdy Mohamed <iosamammohamed@gmail.com>
2020-03-10 16:31:25 +0200

    Added Mvvm - Retrofit - Room - RxAndroid - Repository pattern

--- app/src/main/java/com/osama/movieshow/data/remote/MovieApiInterface.kt
+++ app/src/main/java/com/osama/movieshow/data/remote/MovieApiInterface.kt
@@ -10,18 +10,6 @@ import retrofit2.http.Path
 
 interface MovieApiInterface {
 
-    @GET("${Constants.popular}${Constants.apiKey}")
-    fun getPopularMovies(): Call<JsonObject>
-
-    @GET("${Constants.nowPlaying}${Constants.apiKey}")
-    fun getLatestMovies(): Call<JsonObject>
-
-    @GET("${Constants.topRated}${Constants.apiKey}")
-    fun getTopRatedMovies(): Call<JsonObject>
-
-    @GET("${Constants.upComing}${Constants.apiKey}")
-    fun getUpcomingMovies(): Call<JsonObject>
-
     @GET("{url}${Constants.apiKey}")
     fun getMovies(@Path("url", encoded = true) url:String): Single<MovieResponse>
 
